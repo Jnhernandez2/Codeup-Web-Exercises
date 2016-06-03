c779900b454d3c2756ea8c2353e9dbd85f53e8b0
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-06-03 16:44:21 -0500

    An update on the latest version of an exercise on iterating arrays in javascript.

--- public/JS/iterating.js
+++ public/JS/iterating.js
@@ -6,8 +6,15 @@
     // TODO: Create a log statement that will log the number of elements in the names array.
     console.log("The array is " + names.length + " elements long.");
     // TODO: Create log statements that will print each of the names array elements individually.
-    console.log("The first name is " + names[0] + ".");
-    console.log("The second name is " + names[1] + ".");
-    console.log("The third name is " + names[2] + ".");
-    console.log("The fourth name is " + names[3] + ".");
+    for (var i = 0; i < names.length; i++) {
+    	console.log("The shape at index " + i + " is " + names[i]);
+    }
+
+    names.forEach(function(element, index) {
+    	console.log("The name at " + index + " is " + element);
+    });
+    // console.log("The first name is " + names[0] + ".");
+    // console.log("The second name is " + names[1] + ".");
+    // console.log("The third name is " + names[2] + ".");
+    // console.log("The fourth name is " + names[3] + ".");
 })();
